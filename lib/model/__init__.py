97056a5e32e22f1283a97b9846190a97cc436658
Marcus Olssen <s234816@dtu.dk>
2025-12-01 09:06:56 +0100

    feat: Restructured the pipelines, added MultiTaskLearning, fixed data leakage, and cleaned repository.

--- lib/model/__init__.py
+++ lib/model/__init__.py
@@ -1,4 +1,4 @@
-from .pipelines import (SupervisedNodePredictions, SupervisedEdgePredictions,
+from .pipelines import (SupervisedNodePredictions, SupervisedEdgePredictions, SupervisedMTL,
                         pretrain_gae, pretrain_gmae, train_node_readout, test_node_readout, train_edge_readout, test_edge_readout)
 from .models import (GraphSAGE,
                      make_gae, make_gmae, get_x_dict, Readout, EdgeReadout)
